bae19db30b399fb26544f133933515b17c94c3b7
choi <younggeunn@naver.com>
2025-01-22 22:35:05 +0900

    게시글 CRUD API 구현

---
+++ service/article/src/main/java/demo/board/article/repository/ArticleRepository.java
@@ -0,0 +1,10 @@
+package demo.board.article.repository;
+
+import demo.board.article.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
