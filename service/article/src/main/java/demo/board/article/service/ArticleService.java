86de98b7a40ef7fe664bd38ddd3c2641d7ae265d
choi <younggeunn@naver.com>
2025-02-03 22:29:28 +0900

    게시글 수 구현

--- service/article/src/main/java/demo/board/article/service/ArticleService.java
+++ service/article/src/main/java/demo/board/article/service/ArticleService.java
@@ -1,7 +1,9 @@
 package demo.board.article.service;
 
 import demo.board.article.entity.Article;
+import demo.board.article.entity.BoardArticleCount;
 import demo.board.article.repository.ArticleRepository;
+import demo.board.article.repository.BoardArticleCountRepository;
 import demo.board.article.service.request.ArticleCreateRequest;
 import demo.board.article.service.request.ArticleUpdateRequest;
 import demo.board.article.service.response.ArticlePageResponse;
@@ -19,12 +21,19 @@ public class ArticleService {
 
     private final Snowflake snowflake = new Snowflake();
     private final ArticleRepository articleRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
 
     @Transactional
     public ArticleResponse create(ArticleCreateRequest request) {
         Article article = articleRepository.save(
                 Article.create(snowflake.nextId(), request.getTitle(), request.getContent(), request.getBoardId(), request.getWriterId())
         );
+        int result = boardArticleCountRepository.increase(request.getBoardId());
+        if (result == 0) {
+            boardArticleCountRepository.save(
+                    BoardArticleCount.init(request.getBoardId(), 1L)
+            );
+        }
 
         return ArticleResponse.from(article);
     }
@@ -45,6 +54,7 @@ public class ArticleService {
     public void delete(Long articleId) {
         Article article = articleRepository.findById(articleId).orElseThrow();
         articleRepository.delete(article);
+        boardArticleCountRepository.decrease(article.getBoardId());
     }
 
     public ArticlePageResponse readAll(Long boardId, Long page, Long pageSize) {
@@ -64,6 +74,12 @@ public class ArticleService {
                 articleRepository.findAllInfiniteScroll(boardId, pageSize) :
                 articleRepository.findAllInfiniteScroll(boardId, pageSize, lastArticleId);
         return articles.stream().map(ArticleResponse::from).toList();
+    }
+
+    public Long count(Long boardId) {
+        return boardArticleCountRepository.findById(boardId)
+                .map(BoardArticleCount::getArticleCount)
+                .orElse(0L);
     }
 
 }
