72707db2a679427276dc3b0423b8b52696b0de74
choi <younggeunn@naver.com>
2025-02-07 22:16:27 +0900

    조회수 어뷰징 방지 정책 구현

--- service/view/src/test/java/demo/board/view/api/ViewApiTest.java
+++ service/view/src/test/java/demo/board/view/api/ViewApiTest.java
@@ -18,7 +18,7 @@ public class ViewApiTest {
         for (int i = 0; i < 10000; i++) {
             executorService.submit(() -> {
                 restClient.post()
-                        .uri("/v1/article-views/articles/{articleId}/users/{userId}", 4L, 1L)
+                        .uri("/v1/article-views/articles/{articleId}/users/{userId}", 6L, 1L)
                         .retrieve();
                 latch.countDown();
             });
@@ -27,10 +27,10 @@ public class ViewApiTest {
         latch.await();
 
         Long count = restClient.get()
-                .uri("/v1/article-views/articles/{articleId}/count", 4L)
+                .uri("/v1/article-views/articles/{articleId}/count", 6L)
                 .retrieve()
                 .body(Long.class);
 
-        System.out.println("count = " + count);
+        System.out.println("count = " + count); // 어뷰징 방지 정책이 추가된 경우 count = 1. (1번 밖에 처리가 되지 않기 때문)
     }
 }
