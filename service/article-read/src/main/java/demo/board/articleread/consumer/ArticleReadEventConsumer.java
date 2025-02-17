19a368a245b5eb060edd44fc081147f6fd576363
choi <younggeunn@naver.com>
2025-02-17 22:49:53 +0900

    게시글 조회 최적화 구현 - Consumer & Controller

---
+++ service/article-read/src/main/java/demo/board/articleread/consumer/ArticleReadEventConsumer.java
@@ -0,0 +1,32 @@
+package demo.board.articleread.consumer;
+
+import demo.board.articleread.service.ArticleReadService;
+import demo.board.common.event.Event;
+import demo.board.common.event.EventPayload;
+import demo.board.common.event.EventType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ArticleReadEventConsumer {
+    private final ArticleReadService articleReadService;
+
+    @KafkaListener(topics = {
+            EventType.Topic.DEMO_BOARD_ARTICLE,
+            EventType.Topic.DEMO_BOARD_COMMENT,
+            EventType.Topic.DEMO_BOARD_LIKE
+    })
+    public void listen(String message, Acknowledgment ack) {
+        log.info("[ArticleReadEventConsumer.listen] message={}", message);
+        Event<EventPayload> event = Event.fromJson(message);
+        if (event != null) {
+            articleReadService.handleEvent(event);
+        }
+        ack.acknowledge();
+    }
+}
