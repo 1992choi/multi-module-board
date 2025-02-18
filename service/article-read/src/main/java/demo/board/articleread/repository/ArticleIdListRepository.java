c8248e074d0bc8bf8ee92f0b12c39aad87a37366
choi <younggeunn@naver.com>
2025-02-18 22:56:01 +0900

    게시글 목록 최적화 전략 구현 - 리포지토리

---
+++ service/article-read/src/main/java/demo/board/articleread/repository/ArticleIdListRepository.java
@@ -0,0 +1,66 @@
+package demo.board.articleread.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Range;
+import org.springframework.data.redis.connection.Limit;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ArticleIdListRepository {
+    private final StringRedisTemplate redisTemplate;
+
+    // article-read::board::{boardId}::article-list
+    private static final String KEY_FORMAT = "article-read::board::%s::article-list";
+
+    public void add(Long boardId, Long articleId, Long limit) {
+        redisTemplate.executePipelined((RedisCallback<?>) action -> {
+            StringRedisConnection conn = (StringRedisConnection) action;
+            String key = generateKey(boardId);
+
+            /*
+                zAdd의 score에 0을 넣는 이유.
+                - score는 double 타입이고, 현재 articleId는 long 타입.
+                  따라서 double 범위가 넘게되면 올바르게 계산되지 않음.
+                  그래서 value로 기준을 삼기 위하여 articleId을 PaddedString으로 만들어서 계산하도록 유도.
+             */
+            conn.zAdd(key, 0, toPaddedString(articleId));
+            conn.zRemRange(key, 0, -limit - 1);
+            return null;
+        });
+    }
+
+    public void delete(Long boardId, Long articleId) {
+        redisTemplate.opsForZSet().remove(generateKey(boardId), toPaddedString(articleId));
+    }
+
+    public List<Long> readAll(Long boardId, Long offset, Long limit) {
+        return redisTemplate.opsForZSet()
+                .reverseRange(generateKey(boardId), offset, offset + limit - 1)
+                .stream().map(Long::valueOf).toList();
+    }
+
+    public List<Long> readAllInfiniteScroll(Long boardId, Long lastArticleId, Long limit) {
+        return redisTemplate.opsForZSet().reverseRangeByLex(
+                generateKey(boardId),
+                lastArticleId == null ?
+                        Range.unbounded() :
+                        Range.leftUnbounded(Range.Bound.exclusive(toPaddedString(lastArticleId))),
+                Limit.limit().count(limit.intValue())
+        ).stream().map(Long::valueOf).toList();
+    }
+
+    private String toPaddedString(Long articleId) {
+        // 1234 -> 0000000000000001234
+        return "%019d".formatted(articleId);
+    }
+
+    private String generateKey(Long boardId) {
+        return KEY_FORMAT.formatted(boardId);
+    }
+}
